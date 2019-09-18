45704f6bfa2928ad937e4856fb5d8ec1da57c4de
Dirk <Dirk.Maschke@m4-cp.de>
2019-09-18 11:54:53 +0200

    Update Comon.php

--- Scripts/Comon.php
+++ Scripts/Comon.php
@@ -16,5 +16,23 @@ require_once(substr(__FILE__, 0,strpos(__FILE__, 'adm_plugins')-1).'/adm_program
 $app_url = substr(__DIR__ ). '/adm_plugins/accounting/';
 $app_show = $app_url . 'accounting.php';
 $image_url = $app_url . 'Images/';
+$script_url = 'Scripts/';
 
+  function bar()
+  {
+   return 'Ich existiere nicht, bis foo() aufgerufen wurde.';
+  }
+
+function bbutton ($Link, $Label1, $Label2)
+	{
+	$B1 = '<a href="'.$Link.'" title="Backn">';
+	$B1 = $B1 . '<button name="Klickmich" type="button"';
+	$B1 = $B1 . '  value="SELFHTML">';
+	$B1 = $B1 . ' <p><img src=Scripts/button.php?text=' . $Label1 . ' border="0" alt="'. $Label1 . '/' . $Label2 . '"><br>';
+	$B1 = $B1 . '<b>'.$Label2.'</b></p>';
+	$B1 = $B1 . '</button>';
+	$B1 = $B1 . '</button></a>';
+
+	return $B1;
+}
 ?>
